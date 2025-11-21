4fcbf41defbb14ca9709104edcd8d60a25588677
elshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-11-21 08:42:46 +0200

    add header in home view

--- lib/futcher/home/view/home_view.dart
+++ lib/futcher/home/view/home_view.dart
@@ -1,11 +1,56 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hangry_app/core/constants/app_color.dart';
+import 'package:hangry_app/futcher/shared/custem_text.dart';
+import 'package:svg_flutter/svg.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            Gap(60) , 
+        
+            /// header  
+             Row(
+               children: [ 
+
+
+                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                     SvgPicture.asset(
+                      "assets/logo/logo.svg" , 
+                      color: AppColor.primery, 
+                      height: 40,
+                      ), 
+                 
+                      Gap(5) , 
+                 
+                      CustemText(
+                      text: " Hello , Elshaarawy", 
+                      color: Colors.grey.shade500, 
+                       size: 20,
+                       weight: FontWeight.w500 , 
+                       ) ,  
+                  ],
+                 ),  
+                 Spacer() ,
+               
+                CircleAvatar(
+                  radius: 30,
+                ), 
+               ],
+             )  , 
+          
+          ],
+        ),
+      ),
+    );
   }
 }
